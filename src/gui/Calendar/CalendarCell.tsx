566b05fb891b2198379699f403ca1c2edc0eef5f
Ryan Sandoval <ryan_sandoval@live.com>
2023-06-03 22:37:10 -0700

    Add calendar date selection

--- src/gui/Calendar/CalendarCell.tsx
+++ src/gui/Calendar/CalendarCell.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import moment from "moment";
+import React, { useCallback } from "react";
 import styled, { css } from "styled-components";
 
-const StyledCell = styled.td<{ muted?: boolean }>`
+const StyledCell = styled.td<{ muted?: boolean; selected?: boolean }>`
   text-align: center;
   background-color: var(--joplin-background-color);
   border-radius: 0.325rem;
@@ -14,18 +15,35 @@ const StyledCell = styled.td<{ muted?: boolean }>`
         `
       : null}
 
+  ${(props) =>
+    props.selected
+      ? css`
+          background-color: var(--joplin-background-color-hover3);
+        `
+      : null}
+
   &:hover {
     background-color: var(--joplin-background-color-hover3);
   }
 `;
 
 export interface CalendarCellProps {
-  day: number | string;
+  date: moment.Moment;
   muted?: boolean;
+  selected?: boolean;
+  onSelect?: (date: moment.Moment) => void;
 }
 
-function CalendarCell({ day, muted }: CalendarCellProps) {
-  return <StyledCell muted={muted}>{day}</StyledCell>;
+function CalendarCell({ date, muted, selected, onSelect }: CalendarCellProps) {
+  const onClickCallback = useCallback(() => {
+    onSelect(date);
+  }, [date, onSelect]);
+
+  return (
+    <StyledCell muted={muted} selected={selected} onClick={onClickCallback}>
+      {date.format("D")}
+    </StyledCell>
+  );
 }
 
 export default CalendarCell;
