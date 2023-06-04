64c4935c647837ae0ca6528098c857ee3ee1727d
Ryan Sandoval <ryan_sandoval@live.com>
2023-06-04 04:47:10 -0700

    Update app to use NoteList and React Query

--- src/gui/index.tsx
+++ src/gui/index.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import Calendar from "./Calendar";
 import moment from "moment";
-import NoteList from "./NoteList";
 import styled from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NoteList from "./NoteList";
+
+const queryClient = new QueryClient();
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(moment());
   return (
-    <>
-      <Calendar initialDate={moment()} />
-      <NoteList />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Calendar
+        initialDate={selectedDate}
+        onDateSelect={(date) => setSelectedDate(date)}
+      />
+      <hr />
+      <NoteList currentDate={selectedDate} />
+    </QueryClientProvider>
   );
 }
 
