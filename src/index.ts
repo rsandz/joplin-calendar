fd3a273bb59b596c51db1b36797ab2829002e10d
Ryan Sandoval <ryan_sandoval@live.com>
2023-06-03 22:38:56 -0700

    Add message handling in plugin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import joplin from "api";
 import moment from "moment";
+import MsgType from "@constants/messageTypes";
 
 async function getMonthNotes(date: moment.Moment) {
   const notes = [];
@@ -22,9 +23,26 @@ async function getMonthNotes(date: moment.Moment) {
   return notes;
 }
 
+async function handlePanelMessage(message) {
+  const msgType = message.type;
+  console.debug(`Calendar: Got message: ${JSON.stringify(message)}`);
+  switch (msgType) {
+    case MsgType.GetNotes:
+      if (!message.currentDate) {
+        console.error(`Could not process ${message} since missing date.`);
+        return null;
+      }
+      return await getMonthNotes(moment(message.currentDate, moment.ISO_8601));
+
+    default:
+      throw new Error(`Could not process message: ${message}`);
+  }
+}
+
 joplin.plugins.register({
   onStart: async function () {
     const panel = await joplin.views.panels.create("calendar_panel");
+    await joplin.views.panels.onMessage(panel, handlePanelMessage);
     await joplin.views.panels.addScript(panel, "gui/index.js");
   },
 });
