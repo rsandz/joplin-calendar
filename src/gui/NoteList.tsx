05cdb97ca08f82cbaa99c2bea6325da3b1a36f71
Ryan Sandoval <ryan_sandoval@live.com>
2023-06-03 22:37:30 -0700

    Implement basic note list

--- src/gui/NoteList.tsx
+++ src/gui/NoteList.tsx
@@ -1,9 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import moment from "moment";
 import React from "react";
+import MsgType from "@constants/messageTypes";
 
-export interface NoteListProps {}
+export interface NoteListProps {
+  currentDate: moment.Moment;
+}
 
 function NoteList(props: NoteListProps) {
-  return <h1>Note List:</h1>;
+  const currentDate = props.currentDate.clone();
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["notes"],
+    queryFn: async () => {
+      console.debug(`Requesting notes for ${currentDate.toLocaleString()}`);
+      return await webviewApi.postMessage({
+        type: MsgType.GetNotes,
+        currentDate: currentDate.toISOString(),
+      });
+    },
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  console.log(data);
+  return (
+    <>
+      <h1>{currentDate.format("MM D, YYYY")}</h1>
+      {...data.map((note) => {
+        return <p>{note.title}</p>;
+      })}
+    </>
+  );
 }
 
 export default NoteList;
