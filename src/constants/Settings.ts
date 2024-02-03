be2867e1f5906a2050d064dd5434792865e18828
Ryan Sandoval <ryan_sandoval@live.com>
2024-02-03 14:10:32 -0800

    Add setting for week start day

--- src/constants/Settings.ts
+++ src/constants/Settings.ts
@@ -1,2 +1,8 @@
 export const SHOW_CALENDAR_BUTTON = "showCalendarToggleOnToolbar";
 export const SHOW_MODIFIED_NOTES = "showModifiedNotes";
+export const WEEK_START_DAY = "weekStartDay";
+
+export enum WeekStartDay {
+  Sunday = "Sunday",
+  Monday = "Monday",
+}
