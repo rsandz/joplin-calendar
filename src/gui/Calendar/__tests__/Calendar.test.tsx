0993dc24d01305b7712cf69bdd7b524d5c59530f
Ryan Sandoval <ryan_sandoval@live.com>
2024-02-03 15:40:06 -0800

    Add support to start week on Monday

--- src/gui/Calendar/__tests__/Calendar.test.tsx
+++ src/gui/Calendar/__tests__/Calendar.test.tsx
@@ -6,9 +6,14 @@ import moment from "moment";
 import { act } from "react-dom/test-utils";
 import useGetMonthStatistics from "../../hooks/useGetMonthStatistics";
 import useWebviewApiOnMessage from "../../hooks/useWebViewApiOnMessage";
+import { WeekStartDay } from "@constants/Settings";
+import useOnSettingsChange from "../../hooks/useOnSettingsChange";
 
 jest.mock("../../hooks/useGetMonthStatistics");
 const mockedUseGetMonthStatistics = jest.mocked(useGetMonthStatistics);
+
+jest.mock("../../hooks/useOnSettingsChange");
+const mockedUseOnSettingsChange = jest.mocked(useOnSettingsChange);
 
 global.webviewApi = {
   postMessage: jest.fn(),
@@ -26,6 +31,7 @@ describe("calendar", () => {
       },
       refetch: jest.fn(),
     });
+    mockedUseOnSettingsChange.mockReset();
   });
 
   it("displays dates correctly", () => {
@@ -48,6 +54,28 @@ describe("calendar", () => {
     // Assert June
     for (let i = 1; i <= 10; i++) {
       expect(cells[i + 31].textContent).toEqual(i.toString());
+    }
+  });
+
+  it("displays dates correctly if week starts on Monday", () => {
+    mockedUseOnSettingsChange.mockReturnValue(WeekStartDay.Monday);
+
+    const date = moment("May-29-2023", "MMM-DD-YYYY");
+    render(<Calendar selectedDate={date} />);
+
+    expect(screen.getByText("May 2023")).toBeDefined();
+
+    const cells = screen.getAllByRole("cell");
+    expect(cells).toHaveLength(42); // 7 days * 6 rows
+
+    // Assert May (No April)
+    for (let i = 0; i <= 30; i++) {
+      expect(cells[i].textContent).toEqual((i + 1).toString());
+    }
+
+    // Assert June
+    for (let i = 1; i <= 11; i++) {
+      expect(cells[i + 30].textContent).toEqual(i.toString());
     }
   });
 
